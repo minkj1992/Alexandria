7252d1cb36cc275c7eeb632c42517efc69a872f1
minkj1992 <minkj1992@gmail.com>
2023-09-09 18:45:42 +0900

    test: http validation test

---
+++ services/chat/tests/test_api/test_errors.py
@@ -0,0 +1,33 @@
+import pytest
+from fastapi import FastAPI, Response
+from httpx import AsyncClient
+from loguru import logger
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_wrong_path_error(app: FastAPI):
+    async with AsyncClient(base_url="http://testserver", app=app) as client:
+        response = await client.get("/wrong_path/")
+
+    assert response.status_code == HTTP_404_NOT_FOUND
+
+    error_data = response.json()
+    logger.info(response)
+    assert "errors" in error_data
+
+
+
+async def test_validation_error(app: FastAPI):
+    @app.get("/wrong_path/{param}")
+    def _(param: int) -> None:  # pragma: no cover
+        pass
+
+    async with AsyncClient(base_url="http://testserver", app=app) as client:
+        response = await client.get("/wrong_path/asd")
+
+    assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+
+    error_data = response.json()
+    assert "errors" in error_data
