494c0b2bc0a3b917c79d95504305a6aa7eea0b27
minkj1992 <minkj1992@gmail.com>
2023-09-12 13:56:29 +0900

    chore: remove unused env and deps

--- services/chat/src/config.py
+++ services/chat/src/config.py
@@ -1,22 +1,20 @@
 from functools import lru_cache
 
-from loguru import logger
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     openai_api_key: str
-    redis_host: str = 'redis-stack'
+    redis_host: str = "redis-stack"
     redis_port: int = 6379
-
-    model_config = SettingsConfigDict(env_file=".env")
+    base_url: str
+    serper_api_key: str
 
     @property
     def redis_url(self) -> str:
-        return f'redis://@{self.redis_host}:{self.redis_port}/0'
+        return f"redis://@{self.redis_host}:{self.redis_port}/0"
 
 
 @lru_cache()
 def get_settings() -> Settings:
     return Settings()
-
